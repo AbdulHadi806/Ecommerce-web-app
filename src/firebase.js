ba11cdad20c9793ae01adcc60795a96904e66ef4
Abdul Hadi <abdul.806@yahoo.com>
2022-11-08 20:54:37 +0500

    ss

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,6 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 });
-console.log(process.env.REACT_APP_API_KEY)
 
 export const auth = app.auth();
 export default app;
